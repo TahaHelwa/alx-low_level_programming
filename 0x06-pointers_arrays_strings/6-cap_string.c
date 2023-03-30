7021b175d28a721d4cd76e122024304c8b2f748b
Taha Helwa <tahakhairy01@gmail.com>
2023-03-30 15:12:10 -0700

    SS

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ *
+ * @s: The string to modify
+ *
+ * Return: The modified string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' ||
+		s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' ||
+		s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' ||
+		s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' ||
+		s[i - 1] == '{' || s[i - 1] == '}')
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+	}
+
+	return s;
+}
