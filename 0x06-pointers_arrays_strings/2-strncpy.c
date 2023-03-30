6809c2f691f159351fd4b77df18bb7cbe8716af0
Taha Helwa <tahakhairy01@gmail.com>
2023-03-30 13:34:44 -0700

    SS

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	/*we iterate over the characters of the src string and put it in dest*/
 	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
 
 	for (; i < n; i++)
-        	dest[i] = '\0';
+        	dest[i] = ' ';
 
 	return (dest);
 }
