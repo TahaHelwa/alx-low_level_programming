fcd443d662e771f3c1ce2a7e1ba2a6614e0c273b
Taha Helwa <tahakhairy01@gmail.com>
2023-03-30 15:28:06 -0700

    SS

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a[5] = { 98, 198, 298, 398, 498 };
+	int *p;
+	p = a + 2;
+	*(p + 1) = 98;
+
+	printf("a[2] = %d\n", *(p));
+	return (0);
+}
