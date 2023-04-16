ab77c1d30e50b9eff45af3f4f1e055f17d3287a8
Taha Helwa <tahakhairy01@gmail.com>
2023-04-15 19:36:00 -0700

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the strings passed as arguments to the program
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]); /* Prints the first argument (the program name) */
+	_putchar('\n'); /* Prints a new line character */
+
+	return (0);
+}
