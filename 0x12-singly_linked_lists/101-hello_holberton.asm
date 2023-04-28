66429c06605b8861f802608ce2ae6994341e6207
TahaHelwa <tahakhairy01@gmail.com>
2023-04-28 22:46:23 +0300

    Printf

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,7 @@
-section .data
-	hello db "Hello, Holberton", 0
-	fmt db "%s\n", 0
+#include <stdio.h>
 
-section .text
-	global main
-	extern printf
-
-main:
-	push rbp
-	mov rbp, rsp
-
-	push hello
-	push fmt
-	call printf
-	add rsp, 16
-
-	mov rax, 0
-	pop rbp
-	ret
+int main(void)
+{
+	printf("Hello, Holberton\n");
+	return 0;
+}
