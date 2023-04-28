a762e12af33cd76068bb504d0f129352d67f5668
TahaHelwa <tahakhairy01@gmail.com>
2023-04-28 22:39:55 +0300

    Printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) called_first()
+/**
+ * called_first - prints a message before the main function is executed
+ */
+void __attribute__((constructor)) called_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
